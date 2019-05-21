f6e0fd6f602019bfaf08bd40cd11d9f3542dd82f
Wegs96 <karimsamy11@hotmail.com>
2019-05-21 17:09:17 +0200

    enums moved to IDatabaseConnection

--- IDatabase/Database.cs
+++ IDatabase/Database.cs
@@ -6,65 +6,6 @@ using MySql.Data.MySqlClient;
 
 namespace IDatabase
 {
-    /// <summary>
-    /// Database Provider 
-    /// </summary>
-    public enum Provider
-    {
-        /// <summary>
-        /// Use MySql As Database Provider
-        /// </summary>
-        MySql,
-
-        /// <summary>
-        /// Use SqlServer As Database Provider
-        /// </summary>
-        SqlServer
-    }
-
-    /// <summary>
-    /// The result of database connection establishment attempt.
-    /// </summary>
-    public enum DatabaseConnectionResult
-    {
-        /// <summary>
-        /// The connection string is invalid.
-        /// </summary>
-        InvalidConnectionString,
-
-        /// <summary>
-        /// Could not authenticate. Invalid credentials or the connection is not trusted (MSSQL).
-        /// </summary>
-        AuthenticationFailure,
-
-        /// <summary>
-        /// Invalid username/password (MySql).
-        /// </summary>
-        InvalidLoginData,
-
-        /// <summary>
-        /// Failed due to connection timeout.
-        /// </summary>
-        ConnectionTimeout,
-
-        /// <summary>
-        /// An error has occurred while establishing a connection to the server
-        /// Cannot connect to server
-        /// </summary>
-        ConnectionError,
-
-        /// <summary>
-        /// Unknown Sql Error...
-        /// </summary>
-        UnknownError,
-
-        /// <summary>
-        /// Connection successfully established.
-        /// </summary>
-        Success,
-
-        
-    }
 
     public class DatabaseConnection : IDatabaseConnection
     {
